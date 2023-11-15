bc6b688a03c3650efef67f886a57f15158df5408
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2023-11-14 21:16:32 -0700

    Made code easier to follow. Sorta figured out image contrast issue. Still need to handle RGB/BGR weirdness on inference.

--- obj-det-http/main.cc
+++ obj-det-http/main.cc
@@ -1,98 +1,29 @@
-// #include "libs/base/http_server.h"
-
-// #include "libs/base/led.h"
-// #include "libs/base/strings.h"
-// #include "libs/base/tasks.h"
-// #include "libs/base/utils.h"
-// #include "third_party/freertos_kernel/include/FreeRTOS.h"
-// #include "third_party/freertos_kernel/include/task.h"
-
-// namespace coralmicro {
-// namespace {
-
-// HttpServer::Content UriHandler(const char* path) {
-//   printf("Request received for %s\r\n", path);
-//   std::vector<uint8_t> html;
-//   html.reserve(64);
-//   if (std::strcmp(path, "/hello.html") == 0) {
-//     printf("Hello World!\r\n");
-//     StrAppend(&html, "<html><body>Hello World!</body></html>");
-//     return html;
-//   }
-//   return {};
-// }
-
-// [[noreturn]] void blink_task(void* param) {
-//   auto led_type = static_cast<Led*>(param);
-//   printf("Hello task.\r\n");
-//   bool on = true;
-//   while (true) {
-//     on = !on;
-//     LedSet(*led_type, on);
-//     vTaskDelay(pdMS_TO_TICKS(500));
-//   }
-// }
-
-// void Main() {
-//   printf("HTTP Server Example!\r\n");
-//   // Turn on Status LED to show the board is on.
-//   LedSet(Led::kStatus, true);
-
-//   auto user_led = Led::kUser;
-//   xTaskCreate(&blink_task, "blink_user_led_task", configMINIMAL_STACK_SIZE,
-//               &user_led, kAppTaskPriority, nullptr);
-
-//   printf("Starting server...\r\n");
-//   HttpServer http_server;
-//   http_server.AddUriHandler(UriHandler);
-//   UseHttpServer(&http_server);
-
-//   std::string ip;
-//   if (GetUsbIpAddress(&ip)) {
-//     printf("GO TO:   http://%s/%s\r\n", ip.c_str(), "hello.html");
-//   }
-//   vTaskSuspend(nullptr);
-// }
-
-// }  // namespace
-// }  // namespace coralmicro
-
-// extern "C" void app_main(void* param) {
-//   (void)param;
-//   coralmicro::Main();
-//   vTaskSuspend(nullptr);void Blink(unsigned int num, unsigned int delay_ms) {
-
-
-
-
-
-
 /**
-* Object Detection with HTTP server over USB
-*
-* Perform object detection with the provided TFLite file running on the TPU. 
-* Image and object detection data is streamed to an HTTP server that can be 
-* accessed via Ethernet-over-USB connection.
-*
-* Based on the following code examples from Google LLC:
-*  - https://github.com/google-coral/coralmicro/tree/main/examples/camera_streaming_http
-*  - https://github.com/google-coral/coralmicro/tree/main/examples/detect_objects
-*
-* Author: Shawn Hymel
-* Date: November 12, 2023
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Object Detection with HTTP server over USB
+ *
+ * Perform object detection with the provided TFLite file running on the TPU. 
+ * Image and object detection data is streamed to an HTTP server that can be 
+ * accessed via Ethernet-over-USB connection.
+ *
+ * Based on the following code examples from Google LLC:
+ *  - https://github.com/google-coral/coralmicro/tree/main/examples/camera_streaming_http
+ *  - https://github.com/google-coral/coralmicro/tree/main/examples/detect_objects
+ *
+ * Author: Shawn Hymel
+ * Date: November 12, 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <cstdio>
 #include <vector>
@@ -115,6 +46,8 @@
 #include "third_party/tflite-micro/tensorflow/lite/micro/micro_interpreter.h"
 #include "third_party/tflite-micro/tensorflow/lite/micro/micro_mutable_op_resolver.h"
 
+#define ENABLE_HTTP_SERVER 1
+
 namespace coralmicro {
 namespace {
 
@@ -127,16 +60,19 @@ constexpr int kTensorArenaSize = 8 * 1024 * 1024;
 STATIC_TENSOR_ARENA_IN_SDRAM(tensor_arena, kTensorArenaSize);
 static std::vector<uint8_t> *img_ptr;
 static SemaphoreHandle_t img_mutex;
-static unsigned int img_width;
-static unsigned int img_height;
+static int img_width;
+static int img_height;
 
 /*******************************************************************************
-* Functions
-*/
+ * Functions
+ */
 
 void Blink(unsigned int num, unsigned int delay_ms);
 
-// Handle HTTP requests
+#if ENABLE_HTTP_SERVER
+/**
+ * Handle HTTP requests
+ */
 HttpServer::Content UriHandler(const char* uri) {
 
   // Give client main page
@@ -165,68 +101,7 @@ HttpServer::Content UriHandler(const char* uri) {
   }
   return {};
 }
-
-/**
-* Capture image and perform inference
-*/
-bool DetectFromCamera(
-  tflite::MicroInterpreter* interpreter, 
-  int model_width,
-  int model_height,
-  std::vector<tensorflow::Object>* results,
-  std::vector<uint8>* image
-) {
-
-  // Make sure memory has been allocated for results and image
-  CHECK(results != nullptr);
-  CHECK(image != nullptr);
-
-  // Turn status LED on to let the user know we're taking a photo
-  LedSet(Led::kUser, true);
-
-  // Get pointer to input tensor buffer
-  auto* input_tensor = interpreter->input_tensor(0);
-
-  // Configure image from camera
-  // printf("a: %lu\r\n", xTaskGetTickCount() * (1000 / configTICK_RATE_HZ));
-  CameraFrameFormat fmt{
-    CameraFormat::kRgb,   
-    CameraFilterMethod::kBilinear,
-    CameraRotation::k270, 
-    model_width,
-    model_height,         
-    false,
-    image->data()
-  };
-
-  // Get frame from camera using the configuration we set (~38 ms)
-  if (xSemaphoreTake(img_mutex, portMAX_DELAY) == pdTRUE) {
-    if (!CameraTask::GetSingleton()->GetFrame({fmt})) {
-      return false;
-    }
-    xSemaphoreGive(img_mutex);
-  }
-
-  // Turn status LED off to let the user know we're done taking a photo
-  LedSet(Led::kUser, false);
-
-  // Copy image to input tensor (~6 ms)
-  std::memcpy(
-    tflite::GetTensorData<uint8_t>(input_tensor), 
-    image->data(),
-    image->size()
-  );
-
-  // Perform inference (~65 ms)
-  if (interpreter->Invoke() != kTfLiteOk) {
-    return false;
-  }
-  
-  // Get object detection results (as a vector of Objects)
-  *results = tensorflow::GetDetectionResults(interpreter, 0.6, 5);
-
-  return true;
-}
+#endif
 
 /**
  * Loop forever taking images from the camera and performing inference
@@ -295,29 +170,71 @@ bool DetectFromCamera(
     dtime = timestamp - timestamp_prev;
     timestamp_prev = timestamp;
 
-    // Get image and perform inference
-    printf("Taking photo...\r\n");
-    if (DetectFromCamera(&interpreter, 
-      img_width, 
-      img_height, 
-      &results, 
-      img_ptr)
-    ) {
-      std::string output = "bboxes | dtime: " + std::to_string(dtime) + "\r\n";
-      for (const auto& object : results) {
-        output += "  id: " + std::to_string(object.id) +
-          " | score: " + std::to_string(object.score) +
-          " | xmin: " + std::to_string(object.bbox.xmin) +
-          " | ymin: " + std::to_string(object.bbox.ymin) +
-          " | xmax: " + std::to_string(object.bbox.xmax) +
-          " | ymin: " + std::to_string(object.bbox.ymax) + 
-          "\r\n";
-      }
-      printf("%s", output.c_str());
+    // Turn status LED on to let the user know we're taking a photo
+    LedSet(Led::kUser, true);
 
-    } else {
-      printf("Failed to detect image from camera.\r\n");
+    // Get frame from camera using the configuration we set (~38 ms)
+    if (xSemaphoreTake(img_mutex, portMAX_DELAY) == pdTRUE) {
+
+      // Configure camera image
+      CameraFrameFormat fmt{
+        CameraFormat::kRgb,   
+        CameraFilterMethod::kBilinear,
+        CameraRotation::k270, 
+        img_height,
+        img_width,         
+        false,            // Preserve ratio
+        img_ptr->data(),  // Where the image is saved
+        true              // Auto white balance
+      };
+
+      // Take a photo
+      if (!CameraTask::GetSingleton()->GetFrame({fmt})) {
+        printf("ERROR: Could not capture frame from camera\r\n");
+        continue;
+      }
+
+      // Turn status LED off to let the user know we're done taking a photo
+      LedSet(Led::kUser, false);
+
+      // Copy image to input tensor (~6 ms)
+      std::memcpy(
+        tflite::GetTensorData<uint8_t>(input_tensor), 
+        img_ptr->data(),
+        img_ptr->size()
+      );
+
+      // Unlock critical section
+      xSemaphoreGive(img_mutex);
     }
+
+    // TODO: It's running inference that causes the weird RGB/BGR swap. WTF.
+    // TODO: Maybe try copying image to a separate buffer to put on web page?
+    // TODO: Also, we need to do image scaling. Things to try:
+    //        - Does changing image w/h on camera init crop or scale?
+    //        - Try nearest-neighbor instead of bilinear...does that affect speed?
+    //        - If those don't work, import EI scaling method from their lib
+    // Perform inference (~65 ms)
+    if (interpreter.Invoke() != kTfLiteOk) {
+      printf("ERROR: Inference failed\r\n");
+      continue;
+    }
+    
+    // Get object detection results (as a vector of Objects)
+    results = tensorflow::GetDetectionResults(&interpreter, 0.6, 5);
+
+    // Print bounding box results
+    std::string output = "bboxes | dtime: " + std::to_string(dtime) + "\r\n";
+    for (const auto& object : results) {
+      output += "  id: " + std::to_string(object.id) +
+        " | score: " + std::to_string(object.score) + 
+        " | xmin: " + std::to_string(object.bbox.xmin) +
+        " | ymin: " + std::to_string(object.bbox.ymin) +
+        " | xmax: " + std::to_string(object.bbox.xmax) +
+        " | ymin: " + std::to_string(object.bbox.ymax) + 
+        "\r\n";
+    }
+    printf("%s", output.c_str());
   }
 }
 
@@ -334,8 +251,8 @@ void Blink(unsigned int num, unsigned int delay_ms) {
 }
 
 /*******************************************************************************
-* Main
-*/
+ * Main
+ */
 
 void Main() {
 
@@ -367,6 +284,8 @@ void Main() {
     nullptr
   );
 
+#if ENABLE_HTTP_SERVER
+
   // Initialize IP over USB
   std::string usb_ip;
   if (GetUsbIpAddress(&usb_ip)) {
@@ -378,6 +297,8 @@ void Main() {
   http_server.AddUriHandler(UriHandler);
   UseHttpServer(&http_server);
 
+#endif
+
   // Main will go to sleep
   vTaskSuspend(nullptr);
 }
@@ -386,8 +307,8 @@ void Main() {
 }  // namespace coralmicro
 
 /**
-* Entrypoint
-*/
+ * Entrypoint
+ */
 extern "C" void app_main(void* param) {
   (void)param;
   coralmicro::Main();
